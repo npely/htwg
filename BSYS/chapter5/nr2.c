cd76fecc5dca266ce9b86f17d9e24083137fe5a8
PelyVPN <pelznik@gmail.com>
2019-10-12 18:24:23 +0200

    updated versions of nr2.c and nr3.c

--- BSYS/chapter5/nr2.c
+++ BSYS/chapter5/nr2.c
@@ -9,10 +9,25 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    open("./nr2.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    int fd = open("./nr2.txt", O_WRONLY | O_APPEND);
+    write(fd, "new program start\n", 18);
     int rc = fork();
 
+    if (fd < 0 || rc < 0)
+    {
+        fprintf(stderr ,"failed");
+        return 1;
+    }
+    else if (rc == 0)
+    {
+        write(fd, "Child writing to file nr2.txt\n", 30);
+    }
+    else
+    {
+        write(fd, "Parent writing to file nr2.txt\n", 31);
+    }
     
+    return 0;
 }
---
+++ BSYS/chapter5/nr3.c
@@ -0,0 +1,32 @@
+// Program which lets the parent and child process print different things
+//
+// Author: Niklas Pelz
+// Date: 8.10.19
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <syscall.h>
+
+int main(void)
+{
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("hello, I am the child (pid:%d)\n", getpid());
+    }
+    else 
+    {
+        int rc_wait = wait(NULL);
+        printf("goodbye, I am the parent (pid:%d) (rc_wait:%d)\n", getpid(), rc_wait);
+    }
+    return 0;
+}
