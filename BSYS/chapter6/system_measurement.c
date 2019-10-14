1e14e98b07b351b6ba49f2d00681d63ad1505408
PelyVPN <pelznik@gmail.com>
2019-10-14 13:44:53 +0200

    updated version of sys_measurement

--- BSYS/chapter6/system_measurement.c
+++ BSYS/chapter6/system_measurement.c
@@ -17,31 +17,32 @@ int main(void)
 {
     struct timespec start, stop;
     double result = 0;
+    size_t cycles = 10;
     //clockid_t clk_id;
     //clk_id = CLOCK_REALTIME;
     //char c;
     //int fd = open("./test.txt", O_RDONLY);
     
-        if (clock_gettime(CLOCK_REALTIME, &start) == -1)
-        {
-            fprintf(stderr, "start of measurement failed\n");
-            exit(1);
-        }
+    if (clock_gettime(CLOCK_REALTIME, &start) == -1)
+    {
+        fprintf(stderr, "start of measurement failed\n");
+        exit(1);
+    }
 
-        for (size_t i = 0; i < 100; i++)
-        {
-            read(1, NULL, 0);
-        }
+    for (size_t i = 0; i < cycles; i++)
+    {
+        read(0, NULL, 0);
+    }
 
-        if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
-        {
-            fprintf(stderr, "stopping the measurement failed\n");
-            exit(1);
-        }
+    if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
+    {
+        fprintf(stderr, "stopping the measurement failed\n");
+        exit(1);
+    }
     
-    result = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - stop.tv_nsec);
-    result = result / 100;
-    printf("%.50lf\n", result);
+    result = (double)(stop.tv_sec - start.tv_sec) + ((double)(stop.tv_nsec - start.tv_nsec) / 1000000000L);
+    result = result / (double)cycles;
+    printf("%.9f\n", result);
 
     return 0;
     
