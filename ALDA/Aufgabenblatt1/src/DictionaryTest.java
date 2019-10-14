1b38f0b9115aeecfc17d520d4192a70d3a7db0ff
PelyVPN <pelznik@gmail.com>
2019-10-14 17:06:57 +0200

    updated version of Aufgabenblatt1

---
+++ ALDA/Aufgabenblatt1/src/DictionaryTest.java
@@ -0,0 +1,110 @@
+/*
+ * Test der verscheidenen Dictionary-Implementierungen
+ *
+ * O. Bittel
+ * 6.7.2017
+ */
+
+/**
+ * Static test methods for different Dictionary implementations.
+ * @author oliverbittel
+ */
+public class DictionaryTest {
+
+	/**
+	 * @param args not used.
+	 */
+	public static void main(String[] args)  {
+		
+		testSortedArrayDictionary();
+		testHashDictionary();
+		testBinaryTreeDictionary();
+	}
+
+	private static void testSortedArrayDictionary() {
+		Dictionary<String, String> dict = new SortedArrayDictionary<>();
+		testDict(dict);
+	}
+	
+	private static void testHashDictionary() {
+		Dictionary<String, String> dict = new HashDictionary<>(3);
+		testDict(dict);
+	}
+	
+	private static void testBinaryTreeDictionary() {
+		Dictionary<String, String> dict = new BinaryTreeDictionary<>();
+		testDict(dict);
+        
+        // Test für BinaryTreeDictionary mit prettyPrint 
+        // (siehe Aufgabe 10; Programmiertechnik 2).
+        // Pruefen Sie die Ausgabe von prettyPrint auf Papier nach.
+        BinaryTreeDictionary<Integer, Integer> btd = new BinaryTreeDictionary<>();
+        btd.insert(10, 0);
+        btd.insert(20, 0);
+        btd.insert(30, 0);
+        System.out.println("insert:");
+        btd.prettyPrint();
+
+        btd.insert(40, 0);
+        btd.insert(50, 0);
+        System.out.println("insert:");
+        btd.prettyPrint();
+
+        btd.insert(21, 0);
+        System.out.println("insert:");
+        btd.prettyPrint();
+
+        btd.insert(35, 0);
+        btd.insert(60, 0);
+        System.out.println("insert:");
+        btd.prettyPrint();
+
+        System.out.println("For Each Loop:");
+        for (Dictionary.Entry<Integer, Integer> e : btd) {
+            System.out.println(e.getKey() + ": " + e.getValue());
+        }
+
+        btd.remove(30);
+        System.out.println("remove:");
+        btd.prettyPrint();
+
+        btd.remove(35);
+        btd.remove(40);
+        btd.remove(50);
+        System.out.println("remove:");
+        btd.prettyPrint();
+    }
+	
+	private static void testDict(Dictionary<String, String> dict) {
+		System.out.println("===== New Test Case ========================");
+		System.out.println("test " + dict.getClass());
+		System.out.println(dict.insert("gehen", "go") == null);		// true
+		String s = new String("gehen");
+		System.out.println(dict.search(s) != null);					// true
+		System.out.println(dict.search(s).equals("go"));			// true
+		System.out.println(dict.insert(s, "walk").equals("go"));	// true
+		System.out.println(dict.search("gehen").equals("walk"));	// true
+		System.out.println(dict.remove("gehen").equals("walk"));	// true
+		System.out.println(dict.remove("gehen") == null);			// true
+		dict.insert("starten", "start");
+		dict.insert("gehen", "go");
+		dict.insert("schreiben", "write");
+		dict.insert("reden", "say");
+		dict.insert("arbeiten", "work");
+		dict.insert("lesen", "read");
+		dict.insert("singen", "sing");
+		dict.insert("schwimmen", "swim");
+		dict.insert("rennen", "run");
+		dict.insert("beten", "pray");
+		dict.insert("tanzen", "dance");
+		dict.insert("schreien", "cry");
+		dict.insert("tauchen", "dive");
+		dict.insert("fahren", "drive");
+		dict.insert("spielen", "play");
+		dict.insert("planen", "plan");
+		for (Dictionary.Entry<String, String> e : dict) {
+			System.out.println(e.getKey() + ": " + e.getValue() + " search: " + dict.search(e.getKey()));
+		}
+	}
+	
+}
--- ALDA/Aufgabenblatt1/src/SortedArrayDictionary.java
+++ ALDA/Aufgabenblatt1/src/SortedArrayDictionary.java
@@ -1,2 +1,82 @@
-public class SortedArrayDictionary {
+import java.util.Iterator;
+
+public class SortedArrayDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
+
+    private Entry<K, V>[] dic;
+    private int size;
+    private static final int DEF_CAPACITY = 32;
+
+    public SortedArrayDictionary() {
+        this.dic = new Entry[DEF_CAPACITY];
+        this.size = 0;
+    }
+
+    private int searchKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (dic[i].getKey().equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void ensureCapacity(int newCapacity) {
+        if (newCapacity < size)
+            return;
+        Entry[] old = dic;
+        dic = new Entry[newCapacity];
+        System.arraycopy(old, 0, dic, 0, size);
+    }
+
+    @Override
+    public V insert(K key, V value) {
+        int i = searchKey(key);
+
+        if (i >= 0) {
+            V old = dic[i].getValue();
+            dic[i].setValue(value);
+            return old;
+        }
+
+        if (dic.length == size) {
+            ensureCapacity(2 * size);
+        }
+
+        dic[size] = new Entry<K, V>(key, value);
+        size++;
+        return null;
+    }
+
+    @Override
+    public V search(K key) {
+        int i = searchKey(key);
+
+        if (i >= 0)
+            return dic[i].getValue();
+        else
+            return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        int i = searchKey(key);
+        if (i == -1)
+            return null;
+
+        V r = dic[i].getValue();
+        for (int j = i; j < size-1; j++)
+            dic[j] = dic[j + 1];
+        dic[--size] = null;
+        return r;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Entry> iterator() {
+        return null;
+    }
 }
