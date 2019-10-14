612cd437a1007f78a9c39920d44aa2bf544b6ed5
PelyVPN <pelznik@gmail.com>
2019-10-14 12:06:41 +0200

    updated version of sys_measurement

--- BSYS/chapter6/system_measurement.c
+++ BSYS/chapter6/system_measurement.c
@@ -21,25 +21,25 @@ int main(void)
     //clk_id = CLOCK_REALTIME;
     //char c;
     //int fd = open("./test.txt", O_RDONLY);
-    for (int i = 0; i < 100; i++)
-    {
+    
         if (clock_gettime(CLOCK_REALTIME, &start) == -1)
         {
             fprintf(stderr, "start of measurement failed\n");
             exit(1);
         }
 
-        read(1, NULL, 0);
+        for (size_t i = 0; i < 100; i++)
+        {
+            read(1, NULL, 0);
+        }
 
         if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
         {
             fprintf(stderr, "stopping the measurement failed\n");
             exit(1);
         }
-
-        result += (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - stop.tv_nsec);
-    }
-
+    
+    result = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - stop.tv_nsec);
     result = result / 100;
     printf("%.50lf\n", result);
 
