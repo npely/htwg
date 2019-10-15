50b83963c264e0a19058512e76d1fdbeb1ef134f
PelyVPN <pelznik@gmail.com>
2019-10-15 13:23:45 +0200

    changes made to nr4

--- BSYS/chapter5/nr4.c
+++ BSYS/chapter5/nr4.c
@@ -6,14 +6,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char* argv[])
+int main(void)
 {
     int rc = fork();
 
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+
+    else if (rc == 0)
+    {
     //char* execv_args[] = { "/bin/ls", "-l", NULL};
     //execv(execv_args[0], execv_args);
 
     char* execvp_args[] = { "ls", "-l", NULL};
     execvp(execvp_args[0], execvp_args);
+    }
+
+    else
+    {
+        int rc_wait = wait(NULL);
+        printf("Hello I am the parent (rc_wait:%d)\n", rc_wait);
+    }
+    
+    return 0;
+
 }
