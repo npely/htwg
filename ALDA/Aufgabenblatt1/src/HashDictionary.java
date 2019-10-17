5d45083df816c9b597f1c65ba8f447b763af11b5
PelyVPN <pelznik@gmail.com>
2019-10-18 00:05:51 +0200

    changes made to HashDictionary

--- ALDA/Aufgabenblatt1/src/HashDictionary.java
+++ ALDA/Aufgabenblatt1/src/HashDictionary.java
@@ -1,11 +1,20 @@
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HashDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
 
-    private Entry<K, V> tab;
-    private int size;
-    private static final int DEF_CAPACITY = 32;
+    private LinkedList<Entry<K, V>>[] tab;
+    private int size = 7;
 
+    public HashDictionary() {
+        this.tab = new LinkedList[];
+    }
+
+    private static boolean isPrime() {
+
+    }
 
     private int hash(K key) {
         int adr = key.hashCode();
@@ -15,17 +24,35 @@ public class HashDictionary<K extends Comparable<? super K>, V> implements Dicti
         return adr;
     }
 
+    //private static boolean isPrime
+
 
     @Override
     public V insert(K key, V value) {
-
+        int adr = hash(key);
+            for (var v : tab[adr]) {
+                if (v.getKey().equals(key)) {
+                    V old = v.getValue();
+                    v.setValue(value);
+                    return old;
+                }
+                else {
+                    tab[adr] = new LinkedList<>(key, value);
+                }
+            }
     }
 
     @Override
     public V search(K key) {
-        for (int i = 0; i < size; i++) {
-            if ()
-        }
+        int adr = hash(key);
+            if (tab[adr] == null)
+                return null;
+            else
+                for (var v : tab[adr]) {
+                    if (v.getKey().equals(key))
+                        return v.getValue();
+                }
+        return null;
     }
 
     @Override
@@ -35,7 +62,7 @@ public class HashDictionary<K extends Comparable<? super K>, V> implements Dicti
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
