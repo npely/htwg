ae48ad69782e08758a5d844a499e6b42c3f66899
PelyVPN <pelznik@gmail.com>
2019-10-19 15:43:50 +0200

    updated BSYS and SSS

--- BSYS/chapter6/context_measurement.c
+++ BSYS/chapter6/context_measurement.c
@@ -1,20 +1,30 @@
-#include <sys/wait.h>
+#define _POSIX_C_SOURCE 199309L
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sched.h>
+
+#define BILLION 1000000000L
 
 int main(int argc, char *argv[])
 {
+    struct timespec start, stop;
+
     int pipefd[2];
     pid_t cpid;
     char buf;
+    int rbytes;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    long result;
+    long resultstart;
+    long resultstop;
+
+    cpu_set_t process;
+    int length = sizeof(process);
+
 
     if (pipe(pipefd) == -1)
     {
@@ -26,35 +36,46 @@ int main(int argc, char *argv[])
 
     if (cpid == -1)
     {
-        perror("fork");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "fork failed");
+        exit(1);
     }
 
-    if (cpid == 0)                              /* Child reads from pipe */
-    {    
-        close(pipefd[1]);                       /* Close unused write end */
+    if (clock_gettime(CLOCK_MONOTONIC_RAW, &start) == -1)
+    {
+        fprintf(stderr, "start of measurement failed\n");
+        exit(1);
+    }
 
-        printf("%d read: ", getpid());
-        while (read(pipefd[0], &buf, 1) > 0)
+    if ((sched_setaffinity(0, length, &process)) < 1)
+    {
+        if (cpid == 0)
         {
-            write(STDOUT_FILENO, &buf, 1);
+            close(pipefd[1]);
+            rbytes = read(pipefd[0], buf, sizeof(buf));
+            printf("Read: %s\n", buf);
+            exit(0);
+        }
+        else
+        {
+            close(pipefd[0]);
+            write(pipefd[1], "Test!\n", 8);
+
+            if (clock_gettime(CLOCK_MONOTONIC_RAW, &stop) == -1)
+            {
+                fprintf(stderr, "stopping the measurement failed\n");
+                exit(1);
+            }
+
+            resultstart = (start.tv_sec * BILLION) + start.tv_nsec;
+            resultstop = (stop.tv_sec * BILLION) + stop.tv_nsec;
+
+            result = resultstop - resultstart;
+            printf("time taken by a context switch: %ldns\n", result);
+
+            close(pipefd[1]);
+            wait(NULL);
         }
         
-
-        write(STDOUT_FILENO, "\n", 1);
-        close(pipefd[0]);
-
-        _exit(EXIT_SUCCESS);
-    }
-    else                                                /* Parent writes argv[1] to pipe */
-    {
-        close(pipefd[0]);                               /* Close unused read end */
-
-        write(pipefd[1], argv[1], strlen(argv[1]));
-        close(pipefd[1]);                               /* Reader will see EOF */
-        printf("%d wrote : %s\n", getpid(), argv[1]);
-        wait(NULL);                                     /* Wait for child */
-        exit(EXIT_SUCCESS);
     }
 
     return 0;
--- SSS/pythontest.py
+++ SSS/pythontest.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import numpy
-
 w, h = 5, 5
 Matrix = [[0 for x in range(w)] for y in range(h)]
 
