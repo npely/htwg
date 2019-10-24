b5a78b0f2952f45a337e1fb19b588f42400ca910
PelyVPN <pelznik@gmail.com>
2019-10-24 15:26:34 +0200

    added Python scripts

--- SSS/genfromtext.py
+++ SSS/genfromtext.py
@@ -7,7 +7,32 @@ Created on Mon Oct 21 16:57:33 2019
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = np.genfromtxt("/home/niklas13/Schreibtisch/test.txt", delimiter=",")
 
+liste = []
+
+x = "./WellenformenVersuch1/{}.csv"
+
+# Abstände, also x Achse!!!
+z = ["70","67","65","63","60","57","55","52","50","47"
+     ,"43","40","37","33","30","27","22","18","14","10"]
+
+iliste = [70,67,65,63,60,57,55,52,50,47,43,40,37,33,30,27,22,18,14,10]
+
+# Daten die ins Array eingelesen werden
+for i in range(0, 20):
+    data = (np.genfromtxt(x.format(z[i]), max_rows=1000, delimiter=";", skip_header = 1000, usecols=([1])))
+    # Durchschnitt als Wert der y Achse
+    #np.average(data)
+    #plt.plot(int(z[i]), np.average(data))
+    liste.insert(i, np.average(data))
+    
+# Plotten
 print(data)
+plt.xlabel("Abstand in cm")
+plt.ylabel("Voltage in V")
+plt.title("SSS Abstandssensor")
+plt.plot(iliste, liste, '.b')
+plt.grid(True)
+plt.show()
--- SSS/pythontest.py
+++ SSS/pythontest.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-w, h = 5, 5
-Matrix = [[0 for x in range(w)] for y in range(h)]
+iliste = [70,67,65,63,60,57,55,52,50,47,43,40,37,33,30,27,22,18,14,10]
 
-Matrix[2][1] = 1
-
-print(Matrix[2][1])
+for x in range(0, 20):
+        with open("./WellenformenVersuch1/" + str(iliste[x]) + ".csv", "r+") as f:
+            text = f.read()
+            f.seek(0)
+            f.truncate()
+            f.write(text.replace(',', '.'))
 
 
 
