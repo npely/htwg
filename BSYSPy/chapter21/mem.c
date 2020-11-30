39ec1a782d778df49f4815d5c9aea7c03572e627
Niklas Pelz <niklas.pelz@htwg-konstanz.de>
2020-11-30 13:12:57 +0100

    added chapter21

--- BSYSPy/chapter21/mem.c
+++ BSYSPy/chapter21/mem.c
@@ -19,8 +19,11 @@ int main(int argc, char *argv[]) {
 	fprintf(stderr, "usage: spin <memory (MB)>\n");
 	exit(1);
     }
+
     long long int size = (long long int) atoi(argv[1]);
     long long int size_in_bytes = size * 1024 * 1024;
+
+    double averageBandwidth = 0.0;
 
     printf("allocating %lld bytes (%.2f MB)\n", 
 	   size_in_bytes, size_in_bytes / (1024 * 1024.0));
@@ -41,7 +44,7 @@ int main(int argc, char *argv[]) {
     double time_since_last_print = 2.0; 
     double t = Time_GetSeconds();
     int loop_count = 0;
-    while (1) {
+    while (loop_count < 20) {
 	x[i++] += 1; // main work of loop done here.
 
 	// if we've gone through the whole loop, reset a bunch of stuff
@@ -50,18 +53,17 @@ int main(int argc, char *argv[]) {
 	    double delta_time = Time_GetSeconds() - t;
 	    time_since_last_print += delta_time;
 	    if (time_since_last_print >= 0.2) { // only print every .2 seconds
-		printf("loop %d in %.2f ms (bandwidth: %.2f MB/s)\n", 
+        		printf("loop %d in %.2f ms (bandwidth: %.2f MB/s)\n", 
 		       loop_count, 1000 * delta_time, 
 		       size_in_bytes / (1024.0*1024.0*delta_time));
 		time_since_last_print = 0;
 	    }
-
+    
 	    i = 0;
 	    t = Time_GetSeconds();
 	    loop_count++;
-	}
+	    }
     }
-
     return 0;
 }
 
---
+++ BSYSPy/chapter21/plotMB.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arrange(1, 12)
+
+y = [2104.70, 2104.38, 2106.06, 2104.53, 2106.92, 2109.56]
